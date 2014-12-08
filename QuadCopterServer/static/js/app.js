7fac5d168a0f60255a0cc56a61df1cf242bf7b06
Ohad <ohad.beny@gmail.com>
2014-12-08 22:30:27 +0200

    Refactoring of the app.js and index.html

--- QuadCopterServer/static/js/app.js
+++ QuadCopterServer/static/js/app.js
@@ -1,12 +1,27 @@
-var map;
-var arrayOSM;
-var arrayAerial;
-var baseOSM;
-var baseAerial;
 var isDrawable = false; 
-
+var map;
 var drawToggleElement = document.getElementById("drawToggle");
 var checkpoints = [];
+
+$(document).ready(init)
+
+function init() {
+	var satTileServer = "http://" + location.host + "/tiles/sat/{z}/{x}/{y}.jpg"
+	var satLayer = new ol.layer.Tile({
+	  source: new ol.source.XYZ( {urls: [satTileServer, satTileServer, satTileServer]})
+	})
+	var controls = new ol.Collection([mousePositionControl , new ol.control.FullScreen()])
+	map = new ol.Map({
+		  layers: [satLayer],
+		  target: 'map',
+		  controls:  controls,
+		  view: new ol.View({
+		    center: [3901957, 3838820],
+		    zoom: 10
+		  })
+	});
+}
+
 
 /***********************/
 var mousePositionControl = new ol.control.MousePosition({
@@ -15,35 +30,20 @@ var mousePositionControl = new ol.control.MousePosition({
   // comment the following two lines to have the mouse position
   // be placed within the map.
   className: 'custom-mouse-position',
-  target: document.getElementById('mouse-position'),
+  //target: document.getElementById('mouse-position'),
   undefinedHTML: '&nbsp;'
 });
 
-var tileServer = "http://" + location.host + "/tiles/sat/{z}/{x}/{y}.jpg"
-var raster = new ol.layer.Tile({
-  source: new ol.source.XYZ( {urls: [tileServer, tileServer, tileServer]})
-});
 
 
-var map = new ol.Map({
-  layers: [raster],
-  target: 'map',
-  controls: ol.control.defaults({
-    attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
-      collapsible: false
-    }) 
-  }).extend([mousePositionControl]),
-  view: new ol.View({
-    center: [3901957, 3838820],
-    zoom: 10
-  })
-});
+
+
 
 // The features are not added to a regular vector layer/source,
 // but to a feature overlay which holds a collection of features.
 // This collection is passed to the modify and also the draw
 // interaction, so that both can add or modify features.
-function getOverlay(opacity) {
+function getOverlay() {
     var featureOverlay = new ol.FeatureOverlay({
       style: new ol.style.Style({
         fill: new ol.style.Fill({
@@ -66,7 +66,7 @@ function getOverlay(opacity) {
 }
 
 var modify = new ol.interaction.Modify({
-  features: getOverlay(0.9).getFeatures(),
+  features: getOverlay().getFeatures(),
   // the SHIFT key must be pressed to delete vertices, so
   // that new vertices can be drawn at the same position
   // of existing vertices
@@ -81,7 +81,7 @@ map.addInteraction(modify);
 
 var draw; // global so we can remove it later
 function addInteraction() {
-  var a = getOverlay(0.8);
+  var a = getOverlay();
   draw = new ol.interaction.Draw({
     features: a.getFeatures(),
     type: /** @type {ol.geom.GeometryType} */ (typeSelect.value)
@@ -99,7 +99,6 @@ var onDrawToggle = function() {
 	isDrawable = true;
 	drawToggleElement.textContent = "Turn map drawing off";
 	addInteraction();
-	
 }
 
 function onDrawAdded(event) {
@@ -117,6 +116,7 @@ var typeSelect = document.getElementById('typeSelector');
   map.removeInteraction(draw);
   addInteraction();
 };*/
+
 
 
 
