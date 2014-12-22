0c13f1d93098cd9d7e729439c2c83d0fa1d12db1
Aviad Rom <aviadrom@mail.technion.ac.il>
2014-12-22 23:00:16 +0200

    Added Drone class. Started working on drone data handler

---
+++ QuadCopterServer/Drone.py
@@ -0,0 +1,34 @@
+import Queue
+from datetime import datetime, date, time
+
+# datetime.strptime('2014-12-22 20:30:37', '%Y-%m-%d %H:%M:%S')
+class Drone:
+
+    def __init__(self):
+        self.lastConnection = datetime.utcnow()
+        self.status = {'timestamp': datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')}
+
+    def update(self, json):
+        if self.__is_stale(json):
+            print('stale, will not update')
+            return
+        self.status = json
+        print self.status #ToDo - change to log
+
+    @staticmethod
+    def __string_to_date(str_date):
+        return datetime.strptime(str_date, '%Y-%m-%d %H:%M:%S')
+
+    def __is_stale(self, json):
+        return self.__string_to_date(self.status['timestamp']) >= self.__string_to_date(json['timestamp'])
+
+
+
+d = Drone()
+import time
+time.sleep(2)
+d.update({'timestamp': '2014-12-21 20:30:00'})
+now = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+d.update({'timestamp': now})
+d.update({'timestamp': now})
+d.update({'timestamp': '2014-12-23 20:30:00'})
--- QuadCopterServer/groundControl.py
+++ QuadCopterServer/groundControl.py
@@ -1,12 +1,37 @@
 __author__ = 'Aviad Rom'
 from flask import Flask, url_for, request, jsonify, redirect
+import Drone
 
 app = Flask(__name__)
+drone = Drone()
 #app.debug = True
 
 mock_data = {'1': {'waypoint': 1, 'lat': 30.12345, 'lon': 32.234234, 'height': 30.3, 'orientation': 95},
              '2': {'waypoint': 2, 'lat': 30.12345, 'lon': 32.234234, 'height': 30.4, 'orientation': 100},
              '3': {'waypoint': 3, 'lat': 30.12345, 'lon': 32.234234, 'height': 30.5, 'orientation': 120}}
+
+
+#
+#FROM CLIENT {
+#     'lat': 30.12345,
+#     'lon': 32.234234,
+#     'height': 30.3,
+#     'orientation': 90,
+#     'message': "",
+#     'battery': 100
+# }
+
+
+#
+#TO CLIENT {
+#     'lat': 30.12345,
+#     'lon': 32.234234,
+#     'height': 30.3,
+#     'orientation': 90,
+#     'message': "",
+#     'battery': 100
+# }
+
 
 #mock_iterator = int(1)
 
@@ -24,10 +49,15 @@ def get_tile(tile_type, x, y, z):
 def flight_data():
     method = request.method
     print(method)
-    # data = request.form.get('foo', type=int)
-    #print (foo)
+    data = ""
+    print request.data
+    print request.json
     data = request.get_json()
-    print(str(data))
+    print data["foo"]
+    # try:
+    #     data = request.get_json()
+    # except Exception, e:
+    #     print e
     a = {'foo': 1234}
     #print(data)
     if method == 'POST':
@@ -41,6 +71,41 @@ def flight_data():
     #return render_template
 
 
+# @app.route('/appData', methods=['GET', 'POST'])
+# def client():
+#
+#
+
+
+
+#
+#FROM DRONE {
+#     'lat': 30.12345,
+#     'lon': 32.234234,
+#     'elevation': 30.3,
+#     'bearing': 90,
+#     'battery': 100
+#     'message': "",
+#     'timestamp': now
+# }
+
+
+#
+#TO DRONE {
+#     'lat': 30.12345,
+#     'lon': 32.234234,
+#     'elevation': 30.3,
+#     'orientation': 90,
+#     'message': "",
+#     'battery': 100
+# }
+@app.route('/droneData', methods=['POST'])
+def droneRequest():
+    json = request.get_json()
+    drone.update(json)
+    #push object to "from_drone" queue
+    #return next mission (actual mission or NOP)
+
 def get_flight_data():
     data = mock_data.get('{0}'.format(get_random_mock_data()))
     return data
@@ -49,7 +114,6 @@ def get_random_mock_data():
     import random
     a = [1, 2, 3]
     return random.choice(a)
-
 
 if __name__ == '__main__':
     app.run(port=8080, host='0.0.0.0', threaded=True)
